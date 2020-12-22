768f17ec33d10f7a68a3e86244323a4774b7e708
EkkuJ <ekku.jokinen@aalto.fi>
2020-12-22 16:46:49 +0200

    1.3 - 1.5

--- part1/src/index.js
+++ part1/src/index.js
@@ -15,12 +15,13 @@ return <p>{part} {exercises}</p>
 }
 
 const Content = (props) => {
-  const part1 = props.part1
-  const exercises1 = props.exercises1
-  const part2 = props.part2
-  const exercises2 = props.exercises2
-  const part3 = props.part3
-  const exercises3 = props.exercises3
+  const parts = props.parts
+  const part1 = parts[0].name
+  const exercises1 = parts[0].exercises
+  const part2 = parts[1].name
+  const exercises2 = parts[1].exercises
+  const part3 = parts[2].name
+  const exercises3 = parts[2].exercises
 
   return (
     <div>
@@ -31,29 +32,41 @@ const Content = (props) => {
   )
 }
 
-const Total = () => {
-
+const Total = (props) => {
+  const parts = props.parts
+  let exercises = 0
+  parts.forEach(part => {
+    exercises = exercises + part.exercises
+  });
+  
+  return <p>Number of exercises {exercises}</p>
 }
+
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
+      <Header course={course.name} />
       <Content 
-        part1={part1}
-        part2={part2}
-        part3={part3}
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}/>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+        parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
